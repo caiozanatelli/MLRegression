9bbf9a08beb2e41e31c5fabc9b56df49f02a9ca3
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2019-01-13 13:33:22 -0200

    Creating regression module model

---
+++ src/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.6
+from regression import Regression
+from regression import REG_ALGORITHMS
+import csv
+import numpy as np
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Machine Learning Regression Module')
+    parser.add_argument('-i', '--input', action='store', type=str, required=True,
+                        help='CSV input database path')
+    parser.add_argument('-a', '--algorithm', action='store', type=str,
+                        choices=REG_ALGORITHMS, default='nnet',
+                        help='Regression algorithm to be used for prediction')
+    parser.add_argument('-f', '--features', action='store', type=int, default=8,
+                        help='Number of features in the database')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    # Build model
+    regression = Regression(args.input, args.algorithm, args.features)
+    # Train model
+    regression.fit()
+    # Predict output
+    print(regression.predict([[0.80, 371670.21, 4, 5.5, 3924475904, 4142837760, 6234112, 0.0]]))
---
+++ src/regression.py
@@ -0,0 +1,61 @@
+import numpy as np
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+
+REG_ALGORITHMS = ['nnet', 'svr', 'dt', 'gbrt', 'knn']
+
+class Regression:
+    '''
+    Regression Module implementing selected Machine Learning algorithms.
+
+    TODO: add cross-validation and plot errors for algorithsm comparison (IMPORTANT)
+    '''
+
+    def __init__(self, dbpath, algorithm, nfeatures):
+        self.__features = nfeatures
+        self.__algorithm = algorithm
+        self.__dbpath = dbpath
+        self.__model = None
+
+    def fit(self):
+        X = np.loadtxt(self.__dbpath, delimiter=',', usecols=np.arange(0, self.__features), skiprows=1)
+        y = np.loadtxt(self.__dbpath, delimiter=',', usecols=(-1), skiprows=1)
+
+        if self.__algorithm == 'nnet':   # Neural Network Regression
+            self.__model = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+        elif self.__algorithm == 'svr':  # SVR
+            pass
+        elif self.__algorithm == 'dt':   # Decision Tree
+            pass
+        elif self.__algorithm == 'gbrt': # Gradient Boosted Regression Trees
+            pass
+        elif self.__algorithm == 'knn':  # K-Nearest Neighbors Regression
+            pass
+        else:
+            print('Error. Algorithm not available.')
+            sys.exit(1)
+
+        self.__model.fit(X, y)
+
+    def predict(self, xarray):
+        if not self.__model:
+            print('Error. Algorithm not set.')
+            sys.exit(1)
+        else:
+            return self.__model.predict(xarray)
+
+    @property
+    def features(self):
+        return self.__features
+
+    @property
+    def algorithm(self):
+        return self.__algorithm
+
+    @property
+    def dbpath(self):
+        return self.__dbpath

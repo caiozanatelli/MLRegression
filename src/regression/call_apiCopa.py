9db71130b8b690856a620eb3a8c85a7ed4e75311
Marcos Carvalho <marcosf40@hotmail.com>
2019-02-23 07:37:01 -0300

    host

--- src/regression/call_apiCopa.py
+++ src/regression/call_apiCopa.py
@@ -8,17 +8,19 @@ class APICopa(object):
     def __init__(self):
 
         #URL do Copa com a Porta para acesso RESTful
-        self.copaUrl = 'http://copaserver:8000/'
+        self.copaUrl = 'http://10.0.0.3:8000/'
         #Nome do container do server de video
-        self.containerName = 'landoserver'
-        
-def migrateContainer(poolStart, poolDestiny):
-    print("Starting Migration")
-    self.data = {
-        "container_name": self.containerName,
-        "container_pool": poolStart,
-        "destination_pool": poolDestiny,
-        "operation": "migrate"
-    }
-    result = requests.post(self.copaUrl+'REST/container', self.data).json()
-    print(result) 
+        self.containerName = 'receiver'
+            
+    def migrateContainer(self, poolStart, poolDestiny):
+        print("Pool Start", poolStart)
+        print("Pool Destiny", poolDestiny)
+        print("Starting Migration")
+        self.data = {
+            "container_name": self.containerName,
+            "container_pool": poolStart,
+            "destination_pool": poolDestiny,
+            "operation": "migrate"
+        }
+        result = requests.post(self.copaUrl+'REST/container', self.data).json()
+        print(result) 
